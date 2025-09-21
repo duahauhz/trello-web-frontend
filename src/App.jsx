33bb5b92d984cc5ff42b866d663fc5161b9ab950
duahauhz <ChienDV.B23CE012@stu.ptit.edu.vn>
2025-09-22 01:08:02 +0700

    22092025

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import {
+  useColorScheme,
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
   return (
     <>
+      <ModeToggle />
+      <hr />
       <div>duahaudev</div>
+      <Typography variant="body2" color="text.secondary">Test Typography</Typography> 
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
